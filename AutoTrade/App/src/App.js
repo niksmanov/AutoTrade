69362764c9185ebba05a4e3d6471f345da927a24
Nikola Manov <niks.manov@gmail.com>
2018-11-18 19:04:26 +0200

    Authenticate routes, add Not found page

--- AutoTrade/App/src/App.js
+++ AutoTrade/App/src/App.js
@@ -1,19 +1,56 @@
-﻿import React from 'react';
-import { Route } from 'react-router';
+﻿import React, { Component } from 'react';
+import { Route, Redirect, Switch } from 'react-router';
+import axios from 'axios';
+
 import Layout from './components/Layout/Layout';
+import NotFound from './components/NotFound/NotFound';
 import Home from './components/Home/Home';
 import Register from './components/Account/Register';
 import Login from './components/Account/Login';
-import FetchData from './components/FetchData/FetchData';
+
 import Profile from './components/Profile/Profile';
 
+const PrivateRoute = ({ component: Component, isAuth, ...rest }) => {
+	return <Route {...rest} render={(props) =>
+		isAuth ?
+			<Component {...props} /> :
+			<Redirect to='/login' />
+	} />
+};
 
-export default () => (
-	<Layout>
-		<Route exact path='/' component={Home} />
-		<Route path='/register' component={Register} />
-		<Route path='/login' component={Login} />
-		<Route path='/profile' component={Profile} />
-		<Route path='/fetchdata/:startDateIndex?' component={FetchData} />
-	</Layout>
-);
+
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			isUserAuth: false,
+		};
+	}
+
+	componentWillMount() {
+		axios.get('/user/current')
+			.then(r => { return r.data })
+			.then(response => {
+				this.setState({ isUserAuth: response.succeeded });
+			});
+	}
+
+	render() {
+		let isAuth = this.state.isUserAuth;
+		return (
+			<Layout>
+				<Switch>
+					<Route exact path='/' component={Home} />
+					<Route path='/register' component={Register} />
+					<Route path='/login' component={Login} />
+
+					<PrivateRoute isAuth={isAuth} path="/profile" component={Profile} />
+
+					<Route component={NotFound} />
+				</Switch>
+			</Layout >
+		);
+	}
+}
+
+export default App;
---
+++ AutoTrade/App/src/components/NotFound/NotFound.js
@@ -0,0 +1,16 @@
+﻿import React from 'react';
+import '../../styles/index.css';
+
+const NotFound = () => {
+	return (
+		<div align="center">
+			<h1> Page not found! </h1>
+			<br />
+			<img src="/images/404.png" alt='not-found' className="spacer" style={{ maxWidth: '100%' }} />
+			<br />
+			<a href="/" className="btn btn-primary spacer" > Go to Home page</a>
+		</div>
+	);
+};
+
+export default NotFound;
